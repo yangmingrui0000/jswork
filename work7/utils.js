cc2e47087a003cc577fa8417f6f18205ca0d3007
yangmingrui0000 <2892553393@qq.com>
2019-10-22 19:35:06 +0800

    jswork

--- work7/utils.js
+++ work7/utils.js
@@ -1,7 +1,7 @@
 function bubbleSort(str) {
     let arr = str.split(',')
     for (let i = 0; i < arr.length - 1; i++) {
-        for (let j = 0; j < arr.length - 1 - 1; j++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 [arr[j],arr[j + 1]] = [arr[j + 1],arr[j]]
             }
