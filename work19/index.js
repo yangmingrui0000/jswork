63fe71421f456917c1155135618b1a9cf2badf94
yangmingrui0000 <2892553393@qq.com>
2019-12-24 17:10:37 +0800

    work19

--- work19/index.js
+++ work19/index.js
@@ -1,24 +1,25 @@
 let ws
 function connect(){
-    let server=document.getElementById('server').value
+    let server = document.getElementById('server').value
     ws = new WebSocket(server);
-    ws.onopen=function(){
+    ws.onopen = function(){
         document.getElementById('conn').disabled='disable';
         document.getElementById('disconn').disabled='';
-        let nickname=document.getElementById('nickname').value 
+        let nickname = document.getElementById('nickname').value 
         if(nickname){
             ws.send('nickname|'+nickname)
         }
+
     }
-    ws.onclose=function(){
+    ws.onclose = function(){
         document.getElementById('conn').disable='';
-        document.getElementById('disconn').disable='disable';
+        document.getElementById('disconn').disable= 'disable';
     }
     ws.onmessage=function(event){
         let board = document.getElementById('board')
         let newmsg = document.createElement('div')
         console.log(event.data)
-        newmsg.innerHTML=event.data
+        newmsg.innerHTML = event.data
         board.appendChild(newmsg)
         board.scrollTop=board.scrollHeight;
     }
@@ -29,6 +30,6 @@ function disconnect(){
 }
 
 function send(){
-    let msg=document.getElementById('content').value 
+    let msg = document.getElementById('content').value 
     ws.send(msg)
 }
